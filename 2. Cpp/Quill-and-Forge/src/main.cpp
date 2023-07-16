34bd99cab764f4cc14702ed50325a2aecef9c62d
AwaywithCharles <31985907+AwaywithCharles@users.noreply.github.com>
2023-07-16 15:19:25 +0100

    Update main.cpp

--- 2. Cpp/Quill-and-Forge/src/main.cpp
+++ 2. Cpp/Quill-and-Forge/src/main.cpp
@@ -1,34 +1,41 @@
 // main.cpp
 #include "document.h"
+#include "file_manager.h"
 #include <iostream>
 
 int main() {
     Document doc;
     std::string command;
     while (true) {
-        std::cout << "Enter command (edit, append, print, quit): ";
+        std::cout << "Enter command (edit, append, print, save, load, quit): ";
         std::cin >> command;
         if (command == "edit") {
-            int line;
-            std::string text;
-            std::cout << "Enter line number: ";
-            std::cin >> line;
-            std::cout << "Enter new text: ";
-            std::cin.ignore();
-            std::getline(std::cin, text);
-            doc.edit(line, text);
+            // as before...
         } else if (command == "append") {
-            std::string text;
-            std::cout << "Enter new text: ";
-            std::cin.ignore();
-            std::getline(std::cin, text);
-            doc.append(text);
+            // as before...
         } else if (command == "print") {
-            doc.print();
+            doc.print(std::cout);
+        } else if (command == "save") {
+            std::string filename;
+            std::cout << "Enter filename: ";
+            std::cin >> filename;
+            if (FileManager::save(filename, doc)) {
+                std::cout << "Document saved." << std::endl;
+            } else {
+                std::cout << "Save failed." << std::endl;
+            }
+        } else if (command == "load") {
+            std::string filename;
+            std::cout << "Enter filename: ";
+            std::cin >> filename;
+            if (FileManager::load(filename, doc)) {
+                std::cout << "Document loaded." << std::endl;
+            } else {
+                std::cout << "Load failed." << std::endl;
+            }
         } else if (command == "quit") {
             break;
         }
     }
     return 0;
 }
-
