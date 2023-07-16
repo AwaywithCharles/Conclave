d5f06fc8eab4baf7a578475b27ac15b6bbe544ab
AwaywithCharles <31985907+AwaywithCharles@users.noreply.github.com>
2023-07-16 15:16:55 +0100

    Update document.cpp

--- 2. Cpp/Quill-and-Forge/src/document.cpp
+++ 2. Cpp/Quill-and-Forge/src/document.cpp
@@ -1,1 +1,20 @@
+// document.cpp
+#include "document.h"
+#include <iostream>
+
+void Document::edit(int line, const std::string& text) {
+    if (line < lines.size()) {
+        lines[line] = text;
+    }
+}
+
+void Document::append(const std::string& text) {
+    lines.push_back(text);
+}
+
+void Document::print() const {
+    for (const auto& line : lines) {
+        std::cout << line << std::endl;
+    }
+}
 
