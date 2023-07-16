f75a2db70fb6527f3d830ad86983d8decb750f09
AwaywithCharles <31985907+AwaywithCharles@users.noreply.github.com>
2023-07-16 15:17:18 +0100

    Update main.cpp

--- 2. Cpp/Quill-and-Forge/src/main.cpp
+++ 2. Cpp/Quill-and-Forge/src/main.cpp
@@ -1,1 +1,34 @@
+// main.cpp
+#include "document.h"
+#include <iostream>
+
+int main() {
+    Document doc;
+    std::string command;
+    while (true) {
+        std::cout << "Enter command (edit, append, print, quit): ";
+        std::cin >> command;
+        if (command == "edit") {
+            int line;
+            std::string text;
+            std::cout << "Enter line number: ";
+            std::cin >> line;
+            std::cout << "Enter new text: ";
+            std::cin.ignore();
+            std::getline(std::cin, text);
+            doc.edit(line, text);
+        } else if (command == "append") {
+            std::string text;
+            std::cout << "Enter new text: ";
+            std::cin.ignore();
+            std::getline(std::cin, text);
+            doc.append(text);
+        } else if (command == "print") {
+            doc.print();
+        } else if (command == "quit") {
+            break;
+        }
+    }
+    return 0;
+}
 
