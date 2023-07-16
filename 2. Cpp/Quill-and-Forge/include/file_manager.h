26c3d46cfba8b4e2ef1b336a1b5afe184a87965d
AwaywithCharles <31985907+AwaywithCharles@users.noreply.github.com>
2023-07-16 15:18:08 +0100

    Update file_manager.h

--- 2. Cpp/Quill-and-Forge/include/file_manager.h
+++ 2. Cpp/Quill-and-Forge/include/file_manager.h
@@ -1,1 +1,9 @@
+// file_manager.h
+#include "document.h"
+#include <string>
 
+class FileManager {
+public:
+    static bool save(const std::string& filename, const Document& doc);
+    static bool load(const std::string& filename, Document& doc);
+};
