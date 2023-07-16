77122aeabf008d98378c798a21a88fe0797c800e
AwaywithCharles <31985907+AwaywithCharles@users.noreply.github.com>
2023-07-16 15:16:31 +0100

    Update document.h

--- 2. Cpp/Quill-and-Forge/include/document.h
+++ 2. Cpp/Quill-and-Forge/include/document.h
@@ -1,1 +1,14 @@
+// document.h
+#include <vector>
+#include <string>
+
+class Document {
+public:
+    void edit(int line, const std::string& text);
+    void append(const std::string& text);
+    void print() const;
+
+private:
+    std::vector<std::string> lines;
+};
 
